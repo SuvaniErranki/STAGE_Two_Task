72ce685a18ad194c1e2915aa8b32cc473a26ddf2
SuvaniErranki <92301554+SuvaniErranki@users.noreply.github.com>
2022-08-19 16:55:23 +0530

    Update Stage2(for 12).R

--- Stage2(for 12).R
+++ Stage2(for 12).R
@@ -12,28 +12,32 @@ for (j in 2:13){
     #print(slope)
   }
   
-  #v<-slope[which.max(slope)]
-  #print(v)
-  new<-data.frame()
-  T<-c()
-  S<-c()
-  #print(typeof(v))
   
-  for(t in which.max(slope):53){
-    #print(typeof(slope[t]))
-    if (slope[t] < slope[which.max(slope)]){
-      T<-c(T,t)
-      #print(slope[t])
-      S<-c(S,slope[t])
-      new<-data.frame(S,T)
-    } 
-  }
+  sub_slopes <- slope[which.max(slope):length(slope)]
+    x <- (which.min(abs(sub_slopes-0)) + (length(slope) - length(sub_slopes)))
+    
+    print(file[x,1])
   
-  #print(j)
-  microbial_growth[new[which.min(S),2],1]
-  my_list<-c(my_list,microbial_growth[new[which.min(S),2],1])
+    my_list<-c(my_list,file[x,1])
 }
 my_list
+#Here, my_list gives the set of time points for 
+#each of the 12 samples of the dataset respectively.
+
+#Therefore, all the 12 samples (A1 to A12) enter stationary phase at 11.86028
+
+colnames(file)
+file_dup<-file[,2:13]
+colnames(file_dup)
+
+sample_names<-colnames(file_dup)
+Entry_time<-my_list
+
+results_table<-data.frame(sample_names,Entry_time)
+View(results_table)
+write.csv(results_table,"results_table.csv")
+
+
 
 #Here, my_list gives the set of time points for 
 #each of the 12 samples of the dataset respectively.
